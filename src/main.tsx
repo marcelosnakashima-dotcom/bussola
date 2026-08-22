5303b6b30cfe54da49692e4e25aabadf67f7bfad
marcelosnakashima-dotcom <marcelo.s.nakashima@gmail.com>
2026-08-22 08:46:37 -0300

    feat: register service worker for push notifications

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import App from './App'
 // @ts-ignore
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js').catch(() => {})
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>
 )
